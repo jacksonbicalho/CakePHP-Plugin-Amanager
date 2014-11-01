3524456c776f185404a9fb66dd53e348bec569dd
Jackson Bicalho <jacksonbicalho@gmail.com>
2014-11-01 01:38:30 -0200

    Ajustando link de logout para seguir a rota criada em routes

--- Config/routes.php
+++ Config/routes.php
@@ -4,55 +4,59 @@
  * Usuários
  */
 Router::connect(
-  '/admin/users',
-  array(
-    'plugin'=>'amanager',
-    'controller'=>'users',
-    'action'=>'index'
-  )
+    '/admin/users',
+    array(
+        'plugin'=>'amanager',
+        'controller'=>'users',
+        'action'=>'index'
+    )
 );
 Router::connect(
-  '/admin/users/:action/*',
-  array(
-    'plugin'=>'amanager',
-    'controller'=>'users',
-  )
+    '/admin/users/:action/*',
+    array(
+        'plugin'=>'amanager',
+        'controller'=>'users',
+    )
+);
+Router::connect(
+    '/admin/logout',
+    array('plugin'=>'amanager','controller'=> 'users', 'action'=>'logout')
 );
 
 /**
  * Grupos
  */
 Router::connect(
-  '/admin/groups',
-  array(
-    'plugin'=>'amanager',
-    'controller'=>'groups',
-    'action'=>'index'
-  )
+    '/admin/groups',
+    array(
+        'plugin'=>'amanager',
+        'controller'=>'groups',
+        'action'=>'index'
+    )
 );
 Router::connect(
-  '/admin/groups/:action/*',
-  array(
-    'plugin'=>'amanager',
-    'controller'=>'groups',
-  )
+    '/admin/groups/:action/*',
+    array(
+        'plugin'=>'amanager',
+        'controller'=>'groups',
+    )
 );
 
 /**
  * Regras
  */
 Router::connect(
-  '/admin/rules',
-  array(
-    'plugin'=>'amanager',
-    'controller'=>'rules',
-    'action'=>'index'
-  )
+    '/admin/rules',
+    array(
+        'plugin'=>'amanager',
+        'controller'=>'rules',
+        'action'=>'index'
+    )
 );
 Router::connect(
-  '/admin/rules/:action/*',
-  array(
-    'plugin'=>'amanager',
-    'controller'=>'rules',
-  )
+    '/admin/rules/:action/*',
+    array(
+        'plugin'=>'amanager',
+        'controller'=>'rules',
+    )
 );
